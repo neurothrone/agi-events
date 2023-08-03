a898c725409c8a1d7ee9c5b75790103e24647355
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-03 15:26:37 +0200

    create accessible constants for image assets

---
+++ lib/src/core/constants/assets_constants.dart
@@ -0,0 +1,9 @@
+class AssetsConstant {
+  static const String _basePath = "assets/images";
+
+  static const String signPrintPromotionNorway = "$_basePath/SIPP-no-logo.svg";
+  static const String signPrintPackDenmarkLogo =
+      "$_basePath/SOPDK-Logo-white.svg";
+  static const String signPrintScandinaviaLogo =
+      "$_basePath/SOPSE-Logo-White.svg";
+}
