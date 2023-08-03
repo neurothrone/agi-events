778ffbf633db51f355d4f533ceb3bbc661e5e738
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-03 15:29:22 +0200

    complete add lead design

---
+++ lib/src/features/leads/add_lead/add_lead_form.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/widgets/primary_button.dart';
+import '../../../core/widgets/custom_text_form_field.dart';
+
+class AddLeadForm extends StatefulWidget {
+  const AddLeadForm({
+    super.key,
+  });
+
+  @override
+  State<AddLeadForm> createState() => _AddLeadFormState();
+}
+
+class _AddLeadFormState extends State<AddLeadForm> {
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _companyController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _zipCodeController = TextEditingController();
+  final _cityController = TextEditingController();
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _companyController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _addressController.dispose();
+    _zipCodeController.dispose();
+    _cityController.dispose();
+    super.dispose();
+  }
+
+  void _addLead() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // OriginalTextFormField(
+          //   controller: _lastNameController,
+          //   label: "Last Name",
+          //   hintText: "Enter last name",
+          // ),
+          CustomTextFormField(
+            controller: _firstNameController,
+            label: "First Name",
+            hintText: "Enter first name",
+          ),
+          const SizedBox(height: 20.0),
+          CustomTextFormField(
+            controller: _lastNameController,
+            label: "Last Name",
+            hintText: "Enter last name",
+          ),
+          const SizedBox(height: 20.0),
+          CustomTextFormField(
+            controller: _companyController,
+            label: "Company",
+            hintText: "Enter company",
+          ),
+          const SizedBox(height: 20.0),
+          CustomTextFormField(
+            controller: _emailController,
+            label: "E-mail",
+            hintText: "Enter e-mail",
+          ),
+          const SizedBox(height: 20.0),
+          CustomTextFormField(
+            controller: _phoneController,
+            label: "Phone",
+            hintText: "Enter phone number",
+            isRequired: false,
+          ),
+          const SizedBox(height: 20.0),
+          CustomTextFormField(
+            controller: _addressController,
+            label: "Address",
+            hintText: "Enter address",
+            isRequired: false,
+          ),
+          const SizedBox(height: 20.0),
+          CustomTextFormField(
+            controller: _zipCodeController,
+            label: "Zip code",
+            hintText: "Enter zip code",
+            isRequired: false,
+          ),
+          const SizedBox(height: 20.0),
+          CustomTextFormField(
+            controller: _cityController,
+            label: "City",
+            hintText: "Enter city",
+            isRequired: false,
+          ),
+          const SizedBox(height: 40.0),
+          PrimaryButton(
+            onPressed: _addLead,
+            label: "Next",
+            height: 60.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/features/leads/add_lead/add_lead_sheet.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/widgets/primary_button.dart';
+import 'add_lead_form.dart';
+
+class AddLeadSheet extends StatelessWidget {
+  const AddLeadSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      body: SafeArea(
+        minimum: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Faux indicator to make it intuitive for the user that the
+            // sheet can be dragged vertically
+            const Align(
+              alignment: Alignment.topCenter,
+              child: SheetDragIndicator(),
+            ),
+            const SizedBox(height: 20.0),
+            const AddLeadTitleText(),
+            const Divider(color: Colors.white24),
+            Expanded(
+              child: ListView(
+                children: const [
+                  SizedBox(height: 10.0),
+                  AddLeadForm(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddLeadTitleText extends StatelessWidget {
+  const AddLeadTitleText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "New lead",
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 16.0,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+}
+
+class SheetDragIndicator extends StatelessWidget {
+  const SheetDragIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60.0,
+      height: 5.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
