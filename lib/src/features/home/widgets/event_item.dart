667c1163bd382232c230169a4bd88b19f54d7eac
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-03 15:32:36 +0200

    move EventCard widget

--- lib/src/features/home/widgets/event_item.dart
+++ lib/src/features/home/widgets/event_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'event_card.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class EventItem extends StatelessWidget {
   const EventItem({
@@ -47,6 +47,37 @@ class EventItem extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class EventCard extends StatelessWidget {
+  const EventCard({
+    super.key,
+    required this.imageAsset,
+    this.height = 150.0,
+    this.padding = 10.0,
+    this.borderRadius = 10.0,
+  });
+
+  final String imageAsset;
+  final double height;
+  final double padding;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius),
+        color: Colors.grey.shade800,
+      ),
+      child: SvgPicture.asset(
+        imageAsset,
+        semanticsLabel: "Event logo",
+      ),
     );
   }
 }
