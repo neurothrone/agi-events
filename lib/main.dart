e44eb76dc8dd377bcbc2825681151297ebee2241
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-04 11:11:25 +0200

    refactor to initialize app for firebase and add provider scope to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+import 'src/app.dart';
+import 'src/core/firebase/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
-    const MainApp(),
+    const ProviderScope(
+      child: MainApp(),
+    ),
   );
 }
