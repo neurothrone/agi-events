79401dcd291fed46c3bad68fa944c59ce5bc1b48
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-05 13:23:01 +0200

    make loadJsonFromAssets() more robust

--- lib/src/core/utils/file_utils.dart
+++ lib/src/core/utils/file_utils.dart
@@ -6,14 +6,19 @@ import 'package:flutter/services.dart';
 import '../constants/constants.dart';
 
 Future<Map<String, dynamic>> loadJsonFromAssets(String assetPath) async {
-  String jsonString = await rootBundle.loadString(assetPath);
-  return json.decode(jsonString) as Map<String, dynamic>;
+  try {
+    String jsonString = await rootBundle.loadString(assetPath);
+    return json.decode(jsonString) as Map<String, dynamic>;
+  } catch (_) {
+    debugPrint("❌ -> Failed to load json from file: $assetPath.");
+    return {};
+  }
 }
 
 void loadTestDataFromAssets() {
   final prettyPrint = JsonEncoder.withIndent(" " * 2);
   loadJsonFromAssets(AssetsConstants.localJsonData)
-// .then((Map<String, dynamic> data) => debugPrint(data.toString()))
+      // .then((Map<String, dynamic> data) => debugPrint(data.toString()))
       .then(
         (Map<String, dynamic> data) => debugPrint(prettyPrint.convert(data)),
       )
