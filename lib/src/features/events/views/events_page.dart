ce71376103403a731ca789c7ad35f6778ee837e3
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-05 05:55:57 +0200

    refactor events page and implement fetching of events

--- lib/src/features/events/views/events_page.dart
+++ lib/src/features/events/views/events_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../leads/my_leads/data/events_controller.dart';
+import '../widgets/events_grid.dart';
 import '../widgets/events_page_background.dart';
-import '../widgets/events_page_content.dart';
+import '../widgets/events_page_header.dart';
 
 class EventsPage extends StatelessWidget {
   const EventsPage({super.key});
@@ -14,6 +18,44 @@ class EventsPage extends StatelessWidget {
           EventsPageBackground(),
           EventsPageContent(),
         ],
+      ),
+    );
+  }
+}
+
+class EventsPageContent extends StatelessWidget {
+  const EventsPageContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Consumer(
+        builder: (context, WidgetRef ref, child) {
+          final AsyncValue<List<String>> eventsState = ref.watch(
+            eventsControllerProvider,
+          );
+
+          return eventsState.when(
+            data: (List<String> eventsIds) {
+              return CustomScrollView(
+                slivers: [
+                  const EventsPageHeader(),
+                  EventsGrid(eventIds: eventsIds),
+                ],
+              );
+            },
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            error: (error, __) {
+              return Center(
+                child: Text("❌ -> Error fetching leads. Error: $error"),
+              );
+            },
+          );
+        },
       ),
     );
   }
