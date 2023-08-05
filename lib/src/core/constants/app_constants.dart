a47cbdf62911c36a4b359614781eb41f22881078
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-05 13:00:56 +0200

    add color constants

--- lib/src/core/constants/app_constants.dart
+++ lib/src/core/constants/app_constants.dart
@@ -1,4 +1,48 @@
+import 'package:flutter/material.dart';
+
 class AppConstants {
+  // Strings
   static const String appTitle = "AGI Events";
+
+  // Dimensions
   static const double borderRadius = 10.0;
+
+  // Colors
+  static const Color sheetBackgroundColor = Color(0xFF0C0C0D);
+
+  static const Color dodgerBlue = Color(0xFF1E90FF);
+  static const Color darkSlateBlue = Color(0xFF483D8B);
+
+  static const Color primaryBlue = darkSlateBlue;
+  static const Color secondaryBlue = dodgerBlue;
+
+  // Color.lerp() will only return null if both colors passed to it are null
+  static Color primaryBlueLighter = Color.lerp(
+        primaryBlue,
+        Colors.white,
+        0.1,
+      ) ??
+      primaryBlue;
+  static Color primaryBlueLightest = Color.lerp(
+        primaryBlue,
+        Colors.white,
+        0.3,
+      ) ??
+      primaryBlue;
+
+  // Gradients
+  static const LinearGradient backgroundGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      primaryBlue,
+      secondaryBlue,
+    ],
+  );
+  static const LinearGradient primaryGradient = LinearGradient(
+    colors: [
+      primaryBlue,
+      secondaryBlue,
+    ],
+  );
 }
