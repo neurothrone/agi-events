cabc34a369280ee2d20fa5f8e77e719371bf067e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-05 05:51:50 +0200

    create util to load local data for fake realtime repository

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'src/app.dart';
+import 'src/core/constants/assets_constants.dart';
 import 'src/core/firebase/firebase_options.dart';
+import 'src/core/interfaces/repositories/fake_realtime_repository.dart';
+import 'src/core/utils/utils.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO: temporary for development
+  localJsonData = await loadJsonFromAssets(AssetsConstants.localJsonData);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
