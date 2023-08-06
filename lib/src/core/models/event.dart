d308a1657561e5c03a23a27d67c295c199c7e3dd
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-06 14:44:15 +0200

    modify Event model to include event details

--- lib/src/core/models/event.dart
+++ lib/src/core/models/event.dart
@@ -1,11 +1,46 @@
-import 'lead.dart';
-
 class Event {
   const Event({
     required this.eventId,
-    required this.leads,
+    required this.title,
+    required this.subtitle,
+    required this.image,
   });
 
+  factory Event.fromMap(Map<String, dynamic> map) {
+    return Event(
+      eventId: map["eventId"] as String,
+      title: map["title"] as String,
+      subtitle: map["subtitle"] as String,
+      image: map["image"] as String,
+    );
+  }
+
   final String eventId;
-  final List<Lead> leads;
+  final String title;
+  final String subtitle;
+  final String image;
+
+  @override
+  String toString() {
+    return "Event{"
+        " eventId: $eventId,"
+        " title: $title,"
+        " subtitle: $subtitle,"
+        " image: $image,"
+        "}";
+  }
+
+  Event copyWith({
+    String? eventId,
+    String? title,
+    String? subtitle,
+    String? image,
+  }) {
+    return Event(
+      eventId: eventId ?? this.eventId,
+      title: title ?? this.title,
+      subtitle: subtitle ?? this.subtitle,
+      image: image ?? this.image,
+    );
+  }
 }
