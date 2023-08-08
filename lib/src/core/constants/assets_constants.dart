69c82328bd5435488ba48167661421712d2e1667
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-08 14:15:10 +0200

    fix paths

--- lib/src/core/constants/assets_constants.dart
+++ lib/src/core/constants/assets_constants.dart
@@ -1,10 +1,12 @@
 class AssetsConstants {
   const AssetsConstants._();
 
-  static const String fakeRealtimeJson = "assets/data/fake-database-data.json";
-  static const String eventsJson = "assets/data/events.json";
-
+  static const String _dataBasePath = "assets/data";
   static const String _imagesBasePath = "assets/images";
 
-  static String imagePath(String imageName) => "$_imagesBasePath$imageName";
+  static const String fakeRealtimeJson =
+      "$_dataBasePath/fake-database-data.json";
+  static const String eventsJson = "$_dataBasePath/events.json";
+
+  static String imagePath(String imageName) => "$_imagesBasePath/$imageName";
 }
