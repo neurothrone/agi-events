fb04f3cc04eac33e4577d70e9bf85d7c9b93074d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-08 17:39:49 +0200

    create a placeholder widget for your events

---
+++ lib/src/features/events/widgets/your_events_placeholder.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class YourEventsPlaceholder extends StatelessWidget {
+  const YourEventsPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20.0, bottom: 40.0),
+        child: RichText(
+          text: const TextSpan(
+            children: [
+              TextSpan(
+                text: "You do not have events yet. Tap on "
+                    "an Event in the ",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0,
+                ),
+              ),
+              TextSpan(
+                text: "Coming Events",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              TextSpan(
+                text: " section and Scan the QR code of "
+                    "your exhibitor badge.",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
