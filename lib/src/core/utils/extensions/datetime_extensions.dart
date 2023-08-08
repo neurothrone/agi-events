a1b518f63c4c03140b042f013c165ef79b8cd443
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-08 06:32:21 +0200

    update formatted and add documentation for formattedForExport

--- lib/src/core/utils/extensions/datetime_extensions.dart
+++ lib/src/core/utils/extensions/datetime_extensions.dart
@@ -2,8 +2,10 @@ import 'package:intl/intl.dart';
 
 extension StringFormat on DateTime {
   /// Formats the [DateTime] into a [String] in the format:
-  /// 14:20, 5 Aug
-  String get formatted => DateFormat("hh:mm, d MMM").format(this);
+  /// 14:20, 05 Aug
+  String get formatted => DateFormat("hh:mm, dd MMM").format(this);
 
+  /// Formats the [DateTime] into a [String] in the format:
+  /// 14:20 Sat 5 Aug 2023
   String get formattedForExport => DateFormat("HH:mm E d MMM y").format(this);
 }
