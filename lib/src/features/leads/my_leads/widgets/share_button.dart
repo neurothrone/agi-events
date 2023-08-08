68b92291b69965c5d525d2edb9bb048fcd334b37
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-08 16:16:58 +0200

    remove padding and set style, semantic label for Share Button

--- lib/src/features/leads/my_leads/widgets/share_button.dart
+++ lib/src/features/leads/my_leads/widgets/share_button.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/constants/app_constants.dart';
+
 class ShareButton extends StatelessWidget {
   const ShareButton({
     super.key,
@@ -15,8 +17,11 @@ class ShareButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: onPressed,
+      padding: EdgeInsets.zero,
       icon: Icon(
-        Platform.isIOS ? CupertinoIcons.share : Icons.share,
+        Platform.isIOS ? CupertinoIcons.share : Icons.share_rounded,
+        color: AppConstants.primaryBlueLightest,
+        semanticLabel: "Share CSV",
       ),
     );
   }
