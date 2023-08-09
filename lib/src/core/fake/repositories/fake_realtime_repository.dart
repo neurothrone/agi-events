64a0b1e5e43a4ba356f1106c599726c1bad08434
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-09 14:41:05 +0200

    remove unused method and simplify code

--- lib/src/core/fake/repositories/fake_realtime_repository.dart
+++ lib/src/core/fake/repositories/fake_realtime_repository.dart
@@ -4,6 +4,8 @@ import '../../interfaces/repositories/realtime_repository.dart';
 import '../../models/models.dart';
 import '../../utils/enums/enums.dart';
 
+// region Providers
+
 final fakeRealtimeRepositoryProvider =
     Provider.family<FakeRealtimeRepository, AsyncValue<Map<String, dynamic>>>(
         (ref, jsonData) {
@@ -17,38 +19,14 @@ final fakeRealtimeRepositoryProvider =
   }
 });
 
+// endregion
+
 class FakeRealtimeRepository implements RealtimeRepository {
   const FakeRealtimeRepository({
     required Map<String, dynamic> data,
   }) : _data = data;
 
   final Map<String, dynamic> _data;
-
-  @override
-  Future<bool> eventExists(String eventId) async {
-    final List<String> events = await fetchEventIds();
-    return events.contains(eventId);
-  }
-
-  @override
-  Future<Map<String, dynamic>> fetchEventsData() async {
-    return Future.value(_data);
-  }
-
-  @override
-  Future<List<String>> fetchEventIds() async {
-    return Future.value(_data.keys.map((key) => key.toString()).toList());
-  }
-
-  @override
-  Future<Map<String, dynamic>?> fetchEventDataById(String eventId) async {
-    final doesEventExist = await eventExists(eventId);
-    if (!doesEventExist) return null;
-
-    final Map<String, dynamic> eventData =
-        _data[eventId] as Map<String, dynamic>;
-    return eventData;
-  }
 
   @override
   Future<RawExhibitorData?> fetchExhibitorById({
@@ -71,13 +49,34 @@ class FakeRealtimeRepository implements RealtimeRepository {
     return exhibitor;
   }
 
-  Future<Map<String, dynamic>?> _fetchEventDataById(
-      String eventId,
-      ) async {
-    Map<String, dynamic>? eventMap =
-    await fetchEventDataById(
-      eventId,
+  @override
+  Future<RawVisitorData?> fetchVisitorById({
+    required String visitorId,
+    required Event event,
+  }) async {
+    final Map<String, dynamic>? eventMap = await _fetchEventDataById(
+      event.eventId,
     );
+
+    if (eventMap == null) return null;
+
+    final RawVisitorData? visitor = _processMapIntoUserData(
+      eventMap: eventMap,
+      userId: visitorId,
+      userCategory: UserCategory.visitor,
+      factory: RawVisitorData.fromMap,
+    );
+
+    return visitor;
+  }
+
+  Future<Map<String, dynamic>?> _fetchEventDataById(String eventId) async {
+    final eventIds = _data.keys.map((key) => key.toString()).toList();
+    final doesEventExist = eventIds.contains(eventId);
+
+    if (!doesEventExist) return null;
+
+    final eventMap = _data[eventId] as Map<String, dynamic>;
     return eventMap;
   }
 
@@ -115,8 +114,8 @@ class FakeRealtimeRepository implements RealtimeRepository {
   }) {
     try {
       final Map<String, dynamic> allUsersData =
-      (eventMap[userCategory.name] as Map)
-          .map((key, value) => MapEntry(key.toString(), value));
+          (eventMap[userCategory.name] as Map)
+              .map((key, value) => MapEntry(key.toString(), value));
       return allUsersData;
     } catch (exception) {
       return null;
@@ -124,14 +123,14 @@ class FakeRealtimeRepository implements RealtimeRepository {
   }
 
   Map<String, dynamic>? _processUserDataByIdFromUsersMap(
-      Map<String, dynamic> usersMap,
-      String userId,
-      ) {
+    Map<String, dynamic> usersMap,
+    String userId,
+  ) {
     if (!usersMap.containsKey(userId)) return null;
 
     try {
       Map<String, dynamic> userData =
-      usersMap[userId]["data"] as Map<String, dynamic>;
+          usersMap[userId]["data"] as Map<String, dynamic>;
       return userData;
     } catch (exception) {
       return null;
