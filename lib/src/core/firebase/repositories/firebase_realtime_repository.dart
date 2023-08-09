939e1b0a3b3b25b91cbdda200b13434e3d872195
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-09 14:23:19 +0200

    implement fetch logic to work for both exhibitor and visitor

--- lib/src/core/firebase/repositories/firebase_realtime_repository.dart
+++ lib/src/core/firebase/repositories/firebase_realtime_repository.dart
@@ -4,8 +4,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../interfaces/repositories/realtime_repository.dart';
-import '../../models/event.dart';
-import '../../models/raw/raw_exhibitor_data.dart';
+import '../../models/models.dart';
+import '../../utils/enums/enums.dart';
 import '../data/providers.dart';
 
 final firebaseRealtimeRepositoryProvider =
@@ -21,57 +21,66 @@ class FirebaseRealtimeRepository implements RealtimeRepository {
   final FirebaseDatabase _database;
 
   @override
-  Future<bool> eventExists(String eventId) async {
-    List<String> eventIds = await fetchEventIds();
-    return eventIds.contains(eventId);
-  }
+  Future<T?> fetchUserById<T extends RawUserData>({
+    required String userId,
+    required Event event,
+  }) async {
+    final DatabaseReference visitorReference = _database
+        .ref(event.eventId)
+        .child(UserCategory.visitor.name)
+        .child(userId);
 
-  @override
-  Future<Map<String, dynamic>?> fetchEventDataById(String eventId) async {
-    final DatabaseReference reference = _database.ref(eventId);
+    final DatabaseReference exhibitorReference = _database
+        .ref(event.eventId)
+        .child(UserCategory.exhibitor.name)
+        .child(userId);
 
     try {
-      DataSnapshot snapshot = await reference.get();
+      DataSnapshot visitorSnapshot = await visitorReference.get();
 
-      if (snapshot.value is Map) {
-        Map<dynamic, dynamic> data = snapshot.value as Map;
-        return data.map((key, value) => MapEntry(key.toString(), value));
+      if (visitorSnapshot.value is! Map) return null;
+
+      if (visitorSnapshot.value != null) {
+        Map<dynamic, dynamic> data = visitorSnapshot.value as Map;
+
+        final Map<String, dynamic> dataMap = (data["data"] as Map).map(
+          (key, value) => MapEntry(key.toString(), value),
+        );
+        final userMap = dataMap.map(
+          (key, value) => MapEntry(key.toString(), value),
+        );
+
+        debugPrint("ℹ️ -> Visitor userMap: $userMap");
+        return RawVisitorData.fromMap(userMap) as T;
       }
+
+      DataSnapshot exhibitorSnapshot = await exhibitorReference.get();
+
+      if (exhibitorSnapshot.value is! Map) return null;
+
+      if (exhibitorSnapshot.value != null) {
+        Map<dynamic, dynamic> data = exhibitorSnapshot.value as Map;
+
+        final Map<String, dynamic> dataMap = (data["data"] as Map).map(
+              (key, value) => MapEntry(key.toString(), value),
+        );
+        final userMap = dataMap.map(
+              (key, value) => MapEntry(key.toString(), value),
+        );
+
+        debugPrint("ℹ️ -> Exhibitor userMap: $userMap");
+        return RawExhibitorData.fromMap(userMap) as T;
+      }
+
+      // If we reach this point, the userId was not found under either
+      // node or the type T is unsupported.
       return null;
     } catch (e) {
       debugPrint(
-        "❌ -> Failed to fetchEventDataById() with reference: "
-        "${reference.toString()}. Error: $e",
+        "❌ -> Failed to fetchUserById() with userId: $userId. Error: $e",
       );
       return null;
     }
-  }
-
-  @override
-  Future<List<String>> fetchEventIds() async {
-    final DatabaseReference reference = _database.ref();
-
-    try {
-      DataSnapshot snapshot = await reference.get();
-
-      if (snapshot.value is Map) {
-        Map<dynamic, dynamic> data = snapshot.value as Map<dynamic, dynamic>;
-        return data.keys.map((key) => key.toString()).toList();
-      }
-      return [];
-    } catch (e) {
-      debugPrint(
-        "❌ -> Failed to fetchEvents() with top-level reference. "
-        "Error: $e",
-      );
-      return [];
-    }
-  }
-
-  @override
-  Future<Map<String, dynamic>> fetchEventsData() {
-    // TODO: implement fetchEventsData
-    throw UnimplementedError();
   }
 
   @override
@@ -79,8 +88,42 @@ class FirebaseRealtimeRepository implements RealtimeRepository {
     required String exhibitorId,
     required Event event,
   }) async {
+    final data = await _fetchDataById(
+      userCategory: UserCategory.exhibitor,
+      userId: exhibitorId,
+      event: event,
+    );
+
+    if (data != null) {
+      return RawExhibitorData.fromMap(data);
+    }
+    return null;
+  }
+
+  @override
+  Future<RawVisitorData?> fetchVisitorById({
+    required String visitorId,
+    required Event event,
+  }) async {
+    final data = await _fetchDataById(
+      userCategory: UserCategory.visitor,
+      userId: visitorId,
+      event: event,
+    );
+
+    if (data != null) {
+      return RawVisitorData.fromMap(data);
+    }
+    return null;
+  }
+
+  Future<Map<String, dynamic>?> _fetchDataById({
+    required UserCategory userCategory,
+    required String userId,
+    required Event event,
+  }) async {
     final DatabaseReference reference =
-        _database.ref(event.eventId).child("exhibitor").child(exhibitorId);
+        _database.ref(event.eventId).child(userCategory.name).child(userId);
 
     try {
       DataSnapshot snapshot = await reference.get();
@@ -95,12 +138,12 @@ class FirebaseRealtimeRepository implements RealtimeRepository {
           (key, value) => MapEntry(key.toString(), value),
         );
 
-        return RawExhibitorData.fromMap(userMap);
+        return userMap;
       }
       return null;
     } catch (e) {
       debugPrint(
-        "❌ -> Failed to fetchEventDataById() with reference: "
+        "❌ -> Failed to fetch data with reference: "
         "${reference.toString()}. Error: $e",
       );
       return null;
