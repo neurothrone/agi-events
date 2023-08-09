57d740a62e19fd3a62e9598f732219c4ace3f4b2
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-09 19:57:09 +0200

    update hour formatting to 24 h

--- lib/src/core/utils/extensions/datetime_extensions.dart
+++ lib/src/core/utils/extensions/datetime_extensions.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 extension StringFormat on DateTime {
   /// Formats the [DateTime] into a [String] in the format:
   /// 2023/08/05 14:20 => 14:20, 05 Aug
-  String get formatted => DateFormat("hh:mm, dd MMM").format(this);
+  String get formatted => DateFormat("HH:mm, dd MMM").format(this);
 
   /// Formats the [DateTime] into a [String] in the format:
   /// 14:20 Sat 5 Aug 2023
