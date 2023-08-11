ee7a9d922275e10f32fc0724f75d8b69270c4833
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-11 13:18:43 +0200

    refactor to a ConsumerWidget and to use auto_animated animation

--- lib/src/features/events/widgets/events_grid.dart
+++ lib/src/features/events/widgets/events_grid.dart
@@ -1,92 +1,85 @@
 import 'package:flutter/material.dart';
 
+import 'package:auto_animated/auto_animated.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/models/event.dart';
 import '../../../core/widgets/centered_progress_indicator.dart';
 import '../data/events_controller.dart';
 import 'event_grid_tile.dart';
-import 'events_sliver_grid.dart';
 
-class EventsGrid extends ConsumerStatefulWidget {
+class EventsGrid extends ConsumerWidget {
   const EventsGrid({
+    Key? key,
+    required this.onTap,
     required this.filter,
     required this.placeholder,
-    required this.onTap,
-    Key? key,
+    required this.scrollController,
   }) : super(key: key);
 
+  final void Function(Event, BuildContext, WidgetRef) onTap;
   final bool Function(Event) filter;
   final Widget placeholder;
-  final void Function(Event, BuildContext, WidgetRef) onTap;
+  final ScrollController scrollController;
 
   @override
-  ConsumerState createState() => _EventsGridState();
-}
-
-class _EventsGridState extends ConsumerState<EventsGrid>
-    with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _opacityAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
-    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      _animationController,
-    );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final AsyncValue<List<Event>> eventsState = ref.watch(
       eventsControllerProvider,
     );
 
     return eventsState.when(
       data: (List<Event> events) {
-        List<Event> filteredEvents = events.where(widget.filter).toList();
+        List<Event> filteredEvents = events.where(filter).toList();
 
         if (filteredEvents.isEmpty) {
           return SliverToBoxAdapter(
-            child: widget.placeholder,
+            child: placeholder,
           );
         }
 
-        // Start the animation here if it hasn't started yet
-        if (!_animationController.isAnimating &&
-            !_animationController.isCompleted) {
-          _animationController.forward();
-        }
-
-        return EventsSliverGrid(
-          eventsLength: filteredEvents.length,
-          builder: (context, index) {
+        return LiveSliverGrid.options(
+          controller: scrollController,
+          options: const LiveOptions(
+            showItemInterval: Duration(milliseconds: 250),
+            showItemDuration: Duration(milliseconds: 250),
+            visibleFraction: 0.025,
+            reAnimateOnVisibility: false,
+          ),
+          itemBuilder: (
+            BuildContext context,
+            int index,
+            Animation<double> animation,
+          ) {
             final event = filteredEvents[index];
 
-            return AnimatedBuilder(
-              animation: _opacityAnimation,
-              builder: (context, _) {
-                return Opacity(
-                  opacity: _opacityAnimation.value,
-                  child: EventGridTile(
-                    onTap: () => widget.onTap(event, context, ref),
-                    event: event,
-                  ),
-                );
-              },
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(-1.0, 0.0),
+                end: const Offset(0.0, 0.0),
+              ).animate(animation),
+              child: ScaleTransition(
+                scale: Tween<double>(
+                  begin: 0.0,
+                  end: 1.0,
+                ).animate(animation),
+                child: EventGridTile(
+                  onTap: () => onTap(event, context, ref),
+                  event: event,
+                ),
+              ),
             );
           },
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount:
+                MediaQuery.orientationOf(context) == Orientation.portrait
+                    ? 2
+                    : 3,
+            crossAxisSpacing: 20.0,
+            mainAxisSpacing: 20.0,
+            childAspectRatio: 0.65,
+          ),
+          itemCount: filteredEvents.length,
         );
       },
       loading: () => const SliverToBoxAdapter(
