57fdd018c8a6ce06ddd231293ab667ad40057e7c
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-12 22:25:42 +0200

    remove redundant function

--- lib/src/core/utils/file_utils.dart
+++ lib/src/core/utils/file_utils.dart
@@ -3,8 +3,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
-import '../constants/constants.dart';
-
 Future<Map<String, dynamic>> loadJsonFromAssets(String assetPath) async {
   try {
     String jsonString = await rootBundle.loadString(assetPath);
@@ -13,16 +11,4 @@ Future<Map<String, dynamic>> loadJsonFromAssets(String assetPath) async {
     debugPrint("❌ -> Failed to load json from file: $assetPath.");
     return {};
   }
-}
-
-void loadTestDataFromAssets() {
-  final prettyPrint = JsonEncoder.withIndent(" " * 2);
-  loadJsonFromAssets(AssetsConstants.fakeRealtimeJson)
-      // .then((Map<String, dynamic> data) => debugPrint(data.toString()))
-      .then(
-        (Map<String, dynamic> data) => debugPrint(prettyPrint.convert(data)),
-      )
-      .onError(
-        (error, stackTrace) => debugPrint("❌ -> Error loading json."),
-      );
 }
