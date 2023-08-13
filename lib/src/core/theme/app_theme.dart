c261fd74dacfa56513a34e597e5802fe0912e30f
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-13 18:15:49 +0200

    add documentation comments

--- lib/src/core/theme/app_theme.dart
+++ lib/src/core/theme/app_theme.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 import '../constants/app_constants.dart';
 
+/// Provides a centralized app theme definition.
+///
+/// This class encapsulates the main theme used throughout the app to
+/// ensure consistency in design and appearance. The theme is based on
+/// the dark theme with custom overrides.
 class AppTheme {
+  /// The primary theme used across the app.
+  ///
+  /// This theme is based on the dark theme, with custom modifications,
+  /// such as the scaffold background color and the app bar appearance.
   static ThemeData theme = ThemeData.dark().copyWith(
     useMaterial3: true,
     scaffoldBackgroundColor: Colors.black,
