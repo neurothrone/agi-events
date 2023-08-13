3454349597eb159530f96ccae4da26524e840b61
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-13 18:51:48 +0200

    add documentation notes to interfaces

--- lib/src/core/interfaces/database_repository.dart
+++ lib/src/core/interfaces/database_repository.dart
@@ -1,15 +1,60 @@
 import '../models/models.dart';
 
+/// An abstract repository defining the necessary operations for
+/// interacting with the app's database.
+///
+/// This class serves as a contract that concrete implementations
+/// must adhere to, ensuring consistent behavior across different
+/// database sources.
 abstract class DatabaseRepository {
+  /// Saves the provided event to the database.
+  ///
+  /// Parameters:
+  /// - [event]: The event data to be saved.
+  ///
+  /// Returns:
+  /// A `Future` that completes once the event has been saved.
   Future<void> saveEvent(Event event);
 
+  /// Fetches all events from the database.
+  ///
+  /// Returns:
+  /// A `Future` that resolves to a list of [Event]s.
   Future<List<Event>> fetchEvents();
 
+  /// Saves the provided lead to the database.
+  ///
+  /// Parameters:
+  /// - [lead]: The lead data to be saved.
+  ///
+  /// Returns:
+  /// A `Future` that completes once the lead has been saved.
   Future<void> saveLead(Lead lead);
 
+  /// Fetches all leads associated with the provided event ID.
+  ///
+  /// Parameters:
+  /// - [eventId]: The unique identifier for the event.
+  ///
+  /// Returns:
+  /// A `Future` that resolves to a list of [Lead]s.
   Future<List<Lead>> fetchLeadsByEventId(String eventId);
 
+  /// Updates the details of an existing lead in the database.
+  ///
+  /// Parameters:
+  /// - [lead]: The lead data with updated details.
+  ///
+  /// Returns:
+  /// A `Future` that completes once the lead has been updated.
   Future<void> updateLead(Lead lead);
 
+  /// Deletes the provided lead from the database.
+  ///
+  /// Parameters:
+  /// - [lead]: The lead data to be deleted.
+  ///
+  /// Returns:
+  /// A `Future` that completes once the lead has been deleted.
   Future<void> deleteLead(Lead lead);
 }
--- lib/src/core/interfaces/realtime_repository.dart
+++ lib/src/core/interfaces/realtime_repository.dart
@@ -1,11 +1,39 @@
 import '../models/models.dart';
 
+/// An abstract repository defining the necessary operations for
+/// fetching real-time data.
+///
+/// This class serves as a contract that concrete implementations
+/// must adhere to, ensuring consistent behavior across different
+/// real-time data sources.
 abstract class RealtimeRepository {
+  /// Fetches the data of an exhibitor based on their unique ID and
+  /// the associated event.
+  ///
+  /// Parameters:
+  /// - [exhibitorId]: The unique identifier for the exhibitor.
+  /// - [event]: The event context within which the exhibitor data
+  /// should be fetched.
+  ///
+  /// Returns:
+  /// A `Future` resolving to [RawExhibitorData] or `null` if no
+  /// data is found.
   Future<RawExhibitorData?> fetchExhibitorById({
     required String exhibitorId,
     required Event event,
   });
 
+  /// Fetches the data of a visitor based on their unique ID and the
+  /// associated event.
+  ///
+  /// Parameters:
+  /// - [visitorId]: The unique identifier for the visitor.
+  /// - [event]: The event context within which the visitor data
+  /// should be fetched.
+  ///
+  /// Returns:
+  /// A `Future` resolving to [RawVisitorData] or `null` if no
+  /// data is found.
   Future<RawVisitorData?> fetchVisitorById({
     required String visitorId,
     required Event event,
