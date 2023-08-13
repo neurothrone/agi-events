b372dc5d66eea184ef91283a5d1772a5b12e442c
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-13 18:28:38 +0200

    modify comments and rename extension names to improve semantics

--- lib/src/core/utils/extensions/datetime_extensions.dart
+++ lib/src/core/utils/extensions/datetime_extensions.dart
@@ -1,27 +1,22 @@
 import 'package:intl/intl.dart';
 
-extension StringFormat on DateTime {
-  /// Formats the [DateTime] into a [String] in the format:
-  /// 2023/08/05 14:20 => 14:20, 05 Aug
+extension DateTimeFormatting on DateTime {
+  /// Formats into a [String]: "14:20, 05 Aug" (e.g. for 2023/08/05 14:20).
   String get formatted => DateFormat("HH:mm, dd MMM").format(this);
 
-  /// Formats the [DateTime] into a [String] in the format:
-  /// 14:20 Sat 5 Aug 2023
-  /// 2023/08/05 14:20 => 14:20 Sat 5 Aug 2023
+  /// Formats for export: "14:20 Sat 5 Aug 2023" (e.g. for 2023/08/05 14:20).
   String get formattedForExport => DateFormat("HH:mm E d MMM y").format(this);
 
-  /// Formats the [DateTime] into a [String] in the format:
-  /// 2023/08/05 14:20 => 5
+  /// Extracts the day part: "5" (e.g. for 2023/08/05 14:20).
   String get formattedForDay => DateFormat("d").format(this);
 
-  /// Formats the [DateTime] into a [String] in the format:
-  /// 2023/08/05 14:20 => 5 Aug
+  /// Extracts the day and month: "5 Aug" (e.g. for 2023/08/05 14:20).
   String get formattedForDayAndMonth => DateFormat("d MMM").format(this);
 }
 
-extension DateTimeUtils on DateTime {
-  /// Returns whether the `date` passed in is after this [DateTime]
-  /// in both date and time
+extension DateTimeComparison on DateTime {
+  /// Returns whether the `date` passed in is after this DateTime in
+  /// both date and time.
   bool isNotAfter(DateTime date) {
     return !date.isAfter(this);
   }
