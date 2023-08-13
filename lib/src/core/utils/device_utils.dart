e20e575b270a7aa94ffc9e6bb4ebf3dedbf20b66
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-13 18:50:59 +0200

    add documentation comments and safeguard for non-ios platforms

--- lib/src/core/utils/device_utils.dart
+++ lib/src/core/utils/device_utils.dart
@@ -1,7 +1,26 @@
+import 'package:flutter/foundation.dart';
+
 import 'package:device_info_plus/device_info_plus.dart';
 
+/// Determines if the current device is an iPad.
+///
+/// This function checks if the app is running on an iOS device and
+/// specifically on an iPad model. If the app is running on the web
+/// or another platform, it immediately returns `false`.
+///
+/// Returns:
+/// - `true` if the device is an iPad.
+/// - `false` otherwise or if the platform is web.
+///
+/// Example:
+/// ```dart
+/// final bool isIpad = await isThisDeviceIpad();
+/// ```
 Future<bool> isThisDeviceIpad() async {
-  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-  IosDeviceInfo info = await deviceInfo.iosInfo;
-  return info.model.toLowerCase().contains("ipad");
+  if (!kIsWeb && (defaultTargetPlatform == TargetPlatform.iOS)) {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    IosDeviceInfo info = await deviceInfo.iosInfo;
+    return info.model.toLowerCase().contains("ipad");
+  }
+  return false;
 }
