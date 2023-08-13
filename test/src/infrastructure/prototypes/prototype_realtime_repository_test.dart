a2b5f41d899272939cfb2f556da2f4962d8b6248
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-13 22:19:55 +0200

    change to use constant rather than hard-coded value

--- test/src/infrastructure/prototypes/prototype_realtime_repository_test.dart
+++ test/src/infrastructure/prototypes/prototype_realtime_repository_test.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:agi_events/src/core/constants/assets_constants.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -89,7 +90,7 @@ void main() {
       TestWidgetsFlutterBinding.ensureInitialized();
 
       mockData = await loadJsonFromAssets(
-        "assets/data/prototype-realtime-data.json",
+        AssetsConstants.prototypeRealtimeJson,
       );
       repository = PrototypeRealtimeRepository(data: mockData);
 
