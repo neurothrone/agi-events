fa84244dda28695cdfaf3bd71ff68fe7068c419d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-16 23:45:49 +0200

    change MaterialApp constructor to use AppRouter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'core/constants/constants.dart';
 import 'core/theme/app_theme.dart';
-import 'features/events/views/events_page.dart';
+import 'features/routing/app_router.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
       title: AppConstants.appTitle,
-      home: const EventsPage(),
+      routerConfig: AppRouter.router,
     );
   }
 }
