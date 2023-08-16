82510ffecbebc1fcebc93f947685ca79707712b3
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-08-16 23:44:26 +0200

    add and export an enum for app routes

---
+++ lib/src/core/utils/enums/app_route.dart
@@ -0,0 +1,5 @@
+enum AppRoute {
+  events,
+  leads,
+  leadDetail,
+}
--- lib/src/core/utils/enums/enums.dart
+++ lib/src/core/utils/enums/enums.dart
@@ -1,3 +1,4 @@
+export 'app_route.dart';
 export 'realtime_error.dart';
 export 'scan_type.dart';
 export 'user_category.dart';
